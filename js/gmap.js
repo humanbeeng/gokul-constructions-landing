80237d1e783f6c8de8f2afdf6943cbeb5ec2a697
Nithin <thenithinraj@gmail.com>
2021-03-22 12:13:05 +0530

    Further Development

--- js/gmap.js
+++ js/gmap.js
@@ -1,4 +1,4 @@
-var mapLocation = new google.maps.LatLng(-37.816248, 144.965981); //change coordinates here
+var mapLocation = new google.maps.LatLng(13.070479393005371,77.44473266601562); //change coordinates here
 var marker;
 var map;
 function initialize() {
@@ -25,20 +25,20 @@ function initialize() {
     //change address details here
     var contentString = '<div class="map-info">' 
     + '<div class="map-title">' 
-    + '<div class="brand" href="#"><img alt="" src="images/brand.png"><div class="brand-info"><div class="brand-name">bauhaus</div><div class="brand-text">architecture</div></div></div></div>' 
+    + '<div class="brand" href="#"><img alt="" src="images/brand.png"><div class="brand-info"><div class="brand-name">Gokula Constructions</div><div class="brand-text">dream, design, create. Inspire</div></div></div></div>' 
     + '<div class="map-address-row">'
-    + '  <span class="text"><strong class="text-dark">Melbourne, VIC 3000, Australia,</strong><br>'
-    + '  269 King Str, 05th Floor</span>'
+    + '  <span class="text"><strong class="text-dark">NH-4 Tumkur Road, Bangalore, India.</strong><br>'
+    + ' , Ground Floor</span>'
     + '</div>'
     + '<div>'
     + '   <span class="text"><strong class="text-dark">Phone:</strong> +99 (0) 344 956 4050</span>'
     + '</div>'
     + '<div class="map-address-row">'
     + '   <span class="map-email">'
-    + '      <span class="text"><strong class="text-dark">Email:</strong> info@bauhaus.co</span>'
+    + '      <span class="text"><strong class="text-dark">Email:</strong> gokulaconstructions@gmail.com</span>'
     + '   </span>'
     + '</div>' 
-    + '<p class="gmap-open"><a href="https://www.google.com/maps/@-37.824812,144.965981,14z?hl=ru-RU" target="_blank">Open on Google Maps</a></p></div>';
+    + '<p class="gmap-open"><a href="https://maps.google.com/maps?q=13.070479393005371%2C77.44473266601562&z=17&hl=en" target="_blank">Open on Google Maps</a></p></div>';
     
     
     var infowindow = new google.maps.InfoWindow({
@@ -50,7 +50,7 @@ function initialize() {
     marker = new google.maps.Marker({
         map: map,
         draggable: true,
-        title: 'Bauhaus', //change title here
+        title: 'Gokula Constructions', //change title here
         animation: google.maps.Animation.DROP,
         position: mapLocation
     });
